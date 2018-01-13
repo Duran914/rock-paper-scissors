dbff874bbbb602e42d7f8052ed30ff764a5d58e1
Duran914 <jonathan.duran.100@my.csun.edu>
2018-01-12 21:21:39 -0800

    code-cleanup

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,53 +1,53 @@
 import random
 
-# win messages
-rockBeatScissors = ("You win! Rock breaks scissors!")
-paperBeatRock = ("You win! Paper covers rock!")
-ScissorsBeatPaper = ("You win! Scissors cuts paper.")
+# game messages
+rockBeatScissors = "You win! Rock breaks scissors!"
+paperBeatRock = "You win! Paper covers rock!"
+ScissorsBeatPaper = "You win! Scissors cuts paper."
 
-# Loss messages
-rockLossToPaper = ("Sorry, Paper covers rock.")
-paperLossToScissors = ("Sorry, Scissors cuts paper.")
-scissorsLossToRock = ("Sorry, Rock breaks scissors")
+rockLossToPaper = "Sorry, Paper covers rock."
+paperLossToScissors = "Sorry, Scissors cuts paper."
+scissorsLossToRock = "Sorry, Rock breaks scissors"
 
-# other messages
-tie = ("It's a Tie!")
-unvailed = ("Unvaild choice")
-playAgain = ()
+tie = "It's a Tie!"
+unvailed = "Unvaild choice"
+
+# while loop to play again
+playAgain = ""
 
 while playAgain != "N":
 
-#user input
-    print ("---Lets play Rock, Paper, Scissors---\nType R for rock, P for paper, S for Scissors")
+    #user input
+    print "---Lets play Rock, Paper, Scissors---\nType R for rock, P for paper, S for Scissors"
 
-    userPick = (raw_input("Enter your choice: ").upper())
+    userPick = raw_input("Enter your choice: ").upper()
 
     # Array
     choices = ["R", "P" , "S"]
 
     computerPick = random.choice(choices)
 
-    print ("You chose " + userPick + " The computer chose " + computerPick)
+    print "You chose " + userPick + " The computer chose " + computerPick
 
     if userPick == computerPick:
-        print (tie)
+        print tie
     elif userPick == "R" and computerPick == "P":
-        print (rockLossToPaper)
+        print rockLossToPaper
     elif userPick == "R" and computerPick == "S":
-        print (rockBeatScissors)
+        print rockBeatScissors
     elif userPick == "P" and computerPick == "R":
-        print (paperBeatRock)
+        print paperBeatRock
     elif userPick == "P" and computerPick == "S":
-        print (paperLossToScissors)
+        print paperLossToScissors
     elif userPick == "S" and computerPick == "P":
-        print (ScissorsBeatPaper)
+        print ScissorsBeatPaper
     elif userPick == "S" and computerPick == "R":
-        print (scissorsLossToRock)
+        print scissorsLossToRock
     else:
-        print ("Unvaild choice")
+        print "Unvaild choice"
 
-    playAgain = (raw_input("Play again(Y/N)?").upper())
-    print ("\n")
+    playAgain = raw_input("Play again(Y/N)?").upper()
+    print "\n"
 
 # Ended game
-print ("GoodBye!")
+print "GoodBye!"
